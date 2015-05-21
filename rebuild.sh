252465b730d544aab8ee6f71565c5e5b58255585
Guido Schmutz <guido.schmutz@hotmail.com>
2015-05-21 06:58:52 +0200

    bug fix

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm-base:0.9.4" storm
+docker build -t="wurstmeister/storm-base:0.9.4" storm-base
 docker build -t="wurstmeister/storm:0.9.4" storm
 docker build -t="wurstmeister/storm-nimbus:0.9.4" storm-nimbus
 docker build -t="wurstmeister/storm-supervisor:0.9.4" storm-supervisor
