4742282fe2382518ea793af6e486a20e0ee1a9e6
Guido Schmutz <guido.schmutz@trivadis.com>
2015-06-05 17:14:09 +0200

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm-base:0.9.4" storm-base
-docker build -t="wurstmeister/storm:0.9.4" storm
-docker build -t="wurstmeister/storm-nimbus:0.9.4" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor:0.9.4" storm-supervisor
-docker build -t="wurstmeister/storm-ui:0.9.4" storm-ui
+docker build -t="gschmutz/storm-base:0.9.5" storm-base
+docker build -t="gschmutz/storm:0.9.5" storm
+docker build -t="gschmutz/storm-nimbus:0.9.5" storm-nimbus
+docker build -t="gschmutz/storm-supervisor:0.9.5" storm-supervisor
+docker build -t="gschmutz/storm-ui:0.9.5" storm-ui
